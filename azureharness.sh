1f5f7ddc24c2b3c4079068e7f960ade593aad16c
Dan Rosanova <dnova7@hotmail.com>
2020-03-06 09:11:53 -0800

    Update azureharness.sh

--- azureharness.sh
+++ azureharness.sh
@@ -6,32 +6,45 @@ totalspeed=0
 totalReadSpeed=0
 count=0
 instanceCount=0
+#readInstanceCount=0
+mbpsIn=0
+mbpsOut=0
+
 while read line
-do 
+do
    col1="$(cut -d "," -f 1 <<<"$line")"
    colLen=${#col1}
    if [[ $col1 == "report for"* ]]; then
      ((instanceCount++))
-   
    elif [[ $col1 == *"records sent"* ]]; then
      speedstr="$(cut -d "," -f 2 <<<"$line")"
-     speed="$(cut -d ' ' -f 2 <<<"$speedstr")" 
+     speed="$(cut -d ' ' -f 2 <<<"$speedstr")"
      totalspeed="$(echo "$totalspeed + $speed" | bc -l)"
      ((count++))
-   
+     mbIn="$(cut -d "(" -f 2 <<<"$speedstr")"
+     mbIn="$(cut -d ' ' -f 1  <<<"$mbIn")"
+     mbpsIn="$(echo $mbpsIn + $mbIn | bc -l)"
    elif [[ $colLen == 23 ]]; then
        teststart=$col1
        testend="$(cut -d "," -f 2 <<<"$line")"
-       readSpeed="$(cut -d "," -f 6 <<<"$line")" 
+       readSpeed="$(cut -d "," -f 6 <<<"$line")"
        totalReadSpeed="$(echo "$totalReadSpeed + $readSpeed" | bc -l)"
+       mbOut="$(cut -d "," -f 4 <<<"$line")"
+       mbpsOut="$(echo $mbpsOut+$mbOut | bc -l)"
    elif [[ $col1 == *"Test started at"* ]]; then
-     echo $line '\n'
+     echo $line
    fi
 done < "$1"
 avgspeed="$(echo "$totalspeed/$count" | bc -l)"
 avgread="$(echo "$totalReadSpeed/$count" | bc -l)"
-echo Test start $teststart end $testend Send Average $(printf %.2f "$(echo "$avgspeed*$instanceCount" | bc -l)") messages per second $(printf %2f "$(echo "$avgread*$instanceCount" | bc -l)")  across $instanceCount instances
+
+formattedMBpsIn="$(echo $mbpsIn/$count*$instanceCount | bc -l)"
+formattedMBpsOut="$(echo $mbpsOut/$instanceCount | bc -l)"
+msgRateIn="$(echo $avgspeed*$instanceCount | bc -l)"
+msgRateOut="$(echo $avgread*$instanceCount | bc -l)"
+echo Test start $teststart end $testend Send $(printf %.2f $msgRateIn) msgs/sec Read $(printf %.2f $msgRateOut) Send $(printf %.2f $formattedMBpsIn) MBps Read $(printf %.2f $formattedMBpsOut) MBps across $instanceCount instances
 }
+
 
 topic=mytopic
 count=100000
@@ -75,17 +88,17 @@ rgname=kafkabenchmark$RANDOM
 
 az group create --name $rgname --location eastus
 reportDate=$(date +%Y-%m-%d_%H:%M)
-echo Test started at: $reportDate " " Topic:$topic " " Per Instance Count:$count " " Message Size:$size " " Brokers:$brokers > ${reportDate}_log.txt
+echo "Test started at: $reportDate Topic:$topic Per Instance Message Count:$count Message Size:$size Brokers:$brokers" > ${reportDate}_log.txt
 #start the containers
 for (( i=1; i<=$instances; i++ ))
-do 
+do  
    name=benchmark$i
    az container create --resource-group $rgname --name $name --no-wait --restart-policy Never --image confluentinc/cp-kafka --command-line "/bin/bash -c 'bash <( curl https://raw.githubusercontent.com/djrosanova/kafkabenchmarks/master/benchmark.sh ) -b $brokers -u $username -p $password -t $topic -c $count'"
-done   
+done    
 
 #cycle through containers to get results
 for (( i=1; i<=$instances; i++ ))
-do
+do 
    status=NA
    name=benchmark$i
    until [ $status == "\"Completed\"" ]
@@ -98,6 +111,4 @@ do
 done
 
 #clean up resources
-az group delete --name $rgname -y
-
-report ${reportDate}_log.txt
+az group delete --name $rgname -y 
