4a52b9ef83dee5af1ac0fd943d7423d908100aaa
djrosanova <dnova7@hotmail.com>
2020-03-17 10:35:21 -0700

    adding read ratio

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,8 @@ topic=mytopic
 count=10000
 size=1024
 rate=-1
-while getopts ":t:c:s:r:b:u:p:h:" opt; do
+mode="both"
+while getopts ":t:c:s:r:b:u:p:h:m:" opt; do
   case $opt in
     t) topic="$OPTARG"
     ;;
@@ -20,6 +21,8 @@ while getopts ":t:c:s:r:b:u:p:h:" opt; do
     ;;
     p) password="$OPTARG"
     ;;
+    m) mode="$OPTARG"
+    ;;
 
     h) echo usage
         echo -t topic to send to
@@ -29,6 +32,7 @@ while getopts ":t:c:s:r:b:u:p:h:" opt; do
         echo -b bootstrap servers DNS, with port, for your Confluent Cloud cluster
         echo -u cluster API key
         echo -p cluster API secret
+	echo -m mode: send, receive, both
     ;;
     \?) echo "Invalid option -$OPTARG use -h for help" >&2
     ;;
@@ -39,10 +43,15 @@ if [[ ! -z $bootstrap ]]
 then
     PERF_PRODUCER_CONFIG="bootstrap.servers=$bootstrap\nclient.id=mirror_maker_producer\nrequest.timeout.ms=60000\nsasl.mechanism=PLAIN\nsecurity.protocol=SASL_SSL\nsasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$username\" password=\"$password\";"
     echo -e $PERF_PRODUCER_CONFIG > perf.config
-    nohup kafka-producer-perf-test --topic $topic --record-size $size --producer.config perf.config  --throughput $rate --num-records $count &
-    kafka-consumer-perf-test --broker-list $bootstrap --consumer.config perf.config --topic $topic --messages $count
-#    nohup kafka-consumer-perf-test --broker-list $bootstrap --consumer.config perf.config --topic $topic --messages $count &
-
+    if [[ $mode == "both" ]]
+    then
+      nohup kafka-producer-perf-test --topic $topic --record-size $size --producer.config perf.config  --throughput $rate --num-records $count &
+      kafka-consumer-perf-test --broker-list $bootstrap --consumer.config perf.config --topic $topic --messages $count
+    elif [[ $mode == "send" ]]
+       kafka-producer-perf-test --topic $topic --record-size $size --producer.config perf.config  --throughput $rate --num-records $count &
+    elif [[ $mode == "receive" ]]
+       kafka-consumer-perf-test --broker-list $bootstrap --consumer.config perf.config --topic $topic --messages $count
+    fi
 else
     echo "missing -b bootstrap servers, use -h for help"
 fi
